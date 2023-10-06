ec1a523770f79d9303a92fc37c019729439b8840
Sreenath <sreenathkumar66@gmail.com>
2023-10-07 02:15:00 +0600

    feat: created to add the dynamic component of popup

--- src/features/popup/Popup.tsx
+++ src/features/popup/Popup.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './assets/css/popup.css';
 import SignIn from '../../components/SignIn';
+import PopupContents from './PopupContents';
 
 
-const container = document.createElement('div');
+const container: HTMLElement = document.createElement('div');
 container.setAttribute('id', 'popup-container');
 document.body.appendChild(container);
 const root = createRoot(container);
+
 root.render(
    <>
-      <SignIn />
+      <PopupContents />
    </>
 );
