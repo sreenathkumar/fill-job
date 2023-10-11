5a13708b0dc79b6ece5eedd1e5e71fb862b0069d
Sreenath <sreenathkumar66@gmail.com>
2023-10-12 01:18:05 +0600

    feat: added button action fuction

--- src/features/popup/components/Home.tsx
+++ src/features/popup/components/Home.tsx
@@ -1,17 +1,29 @@
 import React from 'react'
-import { logoutUser } from '../../../api/auth'
 import { ThemeProvider } from '@emotion/react'
-import { Avatar, Box, Button, Grid, Link, Paper, TextField, Typography, Chip, Divider, ButtonGroup } from '@mui/material'
+import { Grid } from '@mui/material'
 import { theme } from '../../../utils/theme'
 import ProfileCard from './ProfileCard'
+import { appView } from '../App'
 
 
 export default function Home() {
+   const handleEditGeneralProfile = () => {
+      appView.value = 'editGeneralProfile'
+   }
+
+   const handleUpdateJobProfile = () => {
+      chrome.tabs.create({ url: 'js/options.html' })
+   }
+
+   const handleFill = () => {
+      console.log('fill job profile');
+   };
+
    return (
       <ThemeProvider theme={theme}>
          <Grid container component="main" gap={'1rem'}>
-            <ProfileCard profile={'General Profile'} name='' bio={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, hic.'} actions={[{ title: 'Edit info', url: '/' }]} />
-            <ProfileCard profile={'Job Profile'} name='' bio='Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, hic.' actions={[{ title: 'Update', url: '/' }, { title: 'Fill up', url: '/' }]} />
+            <ProfileCard profile={'General Profile'} name='' bio={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, hic.'} actions={[{ title: 'Edit info', task: handleEditGeneralProfile },]} />
+            <ProfileCard profile={'Job Profile'} name='' bio='Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, hic.' actions={[{ title: 'Update', task: handleUpdateJobProfile }, { title: 'Fill up', task: handleFill }]} />
          </Grid>
       </ThemeProvider>
    )
