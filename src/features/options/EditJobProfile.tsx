9f751ba1a72e4498fcce51b03475ee72ba0cde57
Sreenath <sreenathkumar66@gmail.com>
2023-10-18 22:21:11 +0600

    feat: check logged in status and get job data from server

--- src/features/options/EditJobProfile.tsx
+++ src/features/options/EditJobProfile.tsx
@@ -1,15 +1,33 @@
 import { Button, SvgIcon, styled } from '@mui/joy'
 import { Avatar, Box, TextField, Grid, Paper, Chip, Divider } from '@mui/material'
 import React, { useState } from 'react'
-import { appView } from '../popup/App';
+import { isLoggedIn } from '../popup/App';
 import { formStructure } from '../../utils/formStructure';
 import FormGroupItem from '../../components/ui/FormGroupItem';
 import FormGroup from '../../components/ui/FormGroup';
-const pdata = require('../../../data.json')
+import { app } from '../../api/auth';
 
-function EditJobProfile({ profData = { ...pdata } }: { profData?: jobProfileDataType }) {
-   const [profileData, setProfileData] = useState(profData || undefined);
-   console.log(profileData['name']);
+
+function EditJobProfile() {
+   const [profileData, setProfileData] = useState<jobProfileDataType>();
+   const user = app.currentUser;//chekcing for user
+   if (user) {
+      const token = localStorage.getItem('token'); // get token from local storage
+      if (token !== null) {
+         const parserdToken = (JSON.parse(token)); // parse token
+         if (parserdToken.expiresAt > Date.now()) { // check if token is valid
+            isLoggedIn.value = true // set logged in to true
+         }
+      }
+   }
+   if (isLoggedIn.value && !profileData) {
+      app.currentUser?.functions.callFunction('getJobProfileData').then((res) => {
+         if (res.jobData !== null) {
+            setProfileData({ ...res.jobData });
+         }
+      });
+   }
+   console.log(isLoggedIn.value);
 
    const handleSubmit = () => { }
    const VisuallyHiddenInput = styled('input')`
@@ -27,16 +45,15 @@ function EditJobProfile({ profData = { ...pdata } }: { profData?: jobProfileData
    return (
       <Box component='form' onSubmit={handleSubmit} sx={{ display: 'flex', flexDirection: 'column', gap: '2.5rem' }} >
          <Grid item xs={12} sm={8} md={5} padding={'1.5rem'} borderRadius={'10px'} elevation={3} component={Paper} square>
-            <Chip label={'Basic Information'} variant="outlined" sx={{ marginBottom: '1rem' }} />
+            <Chip label={'Basic Information'} variant="outlined" sx={{ marginBottom: '2.5rem' }} />
             <Box display={'flex'} flexDirection={'column'} gap={'1rem'}>
                {
                   formStructure.basic_field.map((item, itIndex) => {
                      return <FormGroup key={itIndex} >
                         {
                            item.fields?.map((field, index) => {
-                              const value = profileData[field.id];
                               return (
-                                 <FormGroupItem field={field} key={index} value={profileData ? value : ''} />
+                                 <FormGroupItem field={field} key={index} value={profileData ? profileData[field.id] : ''} />
                               )
                            })
                         }
@@ -47,40 +64,24 @@ function EditJobProfile({ profData = { ...pdata } }: { profData?: jobProfileData
          </Grid>
          <Grid container margin={'0px'} sx={{ gap: '2.5rem' }}>
             <Grid item xs padding={'1.5rem'} borderRadius={'10px'} elevation={3} component={Paper} square>
-               <Chip label={'Present Address'} variant="outlined" sx={{ marginBottom: '1rem' }} />
+               <Chip label={'Present Address'} variant="outlined" sx={{ marginBottom: '2.5rem' }} />
                <Box sx={{ display: 'flex', flexDirection: 'column', gap: '1rem', flexGrow: '1', justifyContent: 'space-between', alignItems: 'center', }}>
                   {
                      formStructure.present_address_field.map((item, itIndex) => {
                         return (
-                           <TextField
-                              key={itIndex}
-                              variant="outlined"
-                              fullWidth
-                              id={item.id}
-                              label={item.label}
-                              name={item.id}
-                              required
-                           />
+                           <FormGroupItem field={item} key={itIndex} value={profileData ? profileData[item.id] : ''} />
                         )
                      })
                   }
                </Box>
             </Grid>
             <Grid item xs padding={'1.5rem'} borderRadius={'10px'} elevation={3} component={Paper} square>
-               <Chip label={'Permanent Address'} variant="outlined" sx={{ marginBottom: '1rem' }} />
+               <Chip label={'Permanent Address'} variant="outlined" sx={{ marginBottom: '2.5rem' }} />
                <Box sx={{ display: 'flex', flexDirection: 'column', gap: '1rem', flexGrow: '1', justifyContent: 'space-between', alignItems: 'center', }}>
                   {
-                     formStructure.present_address_field.map((item, itIndex) => {
+                     formStructure.permanent_address_field.map((item, itIndex) => {
                         return (
-                           <TextField
-                              key={itIndex}
-                              variant="outlined"
-                              fullWidth
-                              id={item.id}
-                              label={item.label}
-                              name={item.id}
-                              required
-                           />
+                           <FormGroupItem field={item} key={itIndex} value={profileData ? profileData[item.id] : ''} />
                         )
                      })
                   }
@@ -89,40 +90,24 @@ function EditJobProfile({ profData = { ...pdata } }: { profData?: jobProfileData
          </Grid>
          <Grid container margin={'0px'} sx={{ gap: '2.5rem' }}>
             <Grid item xs padding={'1.5rem'} borderRadius={'10px'} elevation={3} component={Paper} square>
-               <Chip label={'SSC/Equivalent Level'} variant="outlined" sx={{ marginBottom: '1rem' }} />
+               <Chip label={'SSC/Equivalent Level'} variant="outlined" sx={{ marginBottom: '2.5rem' }} />
                <Box sx={{ display: 'flex', flexDirection: 'column', gap: '1rem', flexGrow: '1', justifyContent: 'space-between', alignItems: 'center', }}>
                   {
                      formStructure.ssc_field.map((item, itIndex) => {
                         return (
-                           <TextField
-                              key={item.id}
-                              variant="outlined"
-                              fullWidth
-                              id={item.id}
-                              label={item.label}
-                              name={item.id}
-                              required
-                           />
+                           <FormGroupItem field={item} key={itIndex} value={profileData ? profileData[item.id] : ''} />
                         )
                      })
                   }
                </Box>
             </Grid>
             <Grid item xs padding={'1.5rem'} borderRadius={'10px'} elevation={3} component={Paper} square>
-               <Chip label={'HSC/Equivalent Level'} variant="outlined" sx={{ marginBottom: '1rem' }} />
+               <Chip label={'HSC/Equivalent Level'} variant="outlined" sx={{ marginBottom: '2.5rem' }} />
                <Box sx={{ display: 'flex', flexDirection: 'column', gap: '1rem', flexGrow: '1', justifyContent: 'space-between', alignItems: 'center', }}>
                   {
                      formStructure.hsc_field.map((item, itIndex) => {
                         return (
-                           <TextField
-                              key={item.id}
-                              variant="outlined"
-                              fullWidth
-                              id={item.id}
-                              label={item.label}
-                              name={item.id}
-                              required
-                           />
+                           <FormGroupItem field={item} key={itIndex} value={profileData ? profileData[item.id] : ''} />
                         )
                      })
                   }
@@ -131,40 +116,24 @@ function EditJobProfile({ profData = { ...pdata } }: { profData?: jobProfileData
          </Grid>
          <Grid container margin={'0px'} sx={{ gap: '2.5rem' }}>
             <Grid item xs padding={'1.5rem'} borderRadius={'10px'} elevation={3} component={Paper} square>
-               <Chip label={'Honors/Equivalent'} variant="outlined" sx={{ marginBottom: '1rem' }} />
+               <Chip label={'Honors/Equivalent'} variant="outlined" sx={{ marginBottom: '2.5rem' }} />
                <Box sx={{ display: 'flex', flexDirection: 'column', gap: '1rem', flexGrow: '1', justifyContent: 'space-between', alignItems: 'center', }}>
                   {
                      formStructure.honors_field.map((item, itIndex) => {
                         return (
-                           <TextField
-                              key={item.id}
-                              variant="outlined"
-                              fullWidth
-                              id={item.id}
-                              label={item.label}
-                              name={item.id}
-                              required
-                           />
+                           <FormGroupItem field={item} key={itIndex} value={profileData ? profileData[item.id] : ''} />
                         )
                      })
                   }
                </Box>
             </Grid>
             <Grid item xs padding={'1.5rem'} borderRadius={'10px'} elevation={3} component={Paper} square>
-               <Chip label={'Masters/Equivalent'} variant="outlined" sx={{ marginBottom: '1rem' }} />
+               <Chip label={'Masters/Equivalent'} variant="outlined" sx={{ marginBottom: '2.5rem' }} />
                <Box sx={{ display: 'flex', flexDirection: 'column', gap: '1rem', flexGrow: '1', justifyContent: 'space-between', alignItems: 'center', }}>
                   {
                      formStructure.masters_field.map((item, itIndex) => {
                         return (
-                           <TextField
-                              key={item.id}
-                              variant="outlined"
-                              fullWidth
-                              id={item.id}
-                              label={item.label}
-                              name={item.id}
-                              required
-                           />
+                           <FormGroupItem field={item} key={itIndex} value={profileData ? profileData[item.id] : ''} />
                         )
                      })
                   }
