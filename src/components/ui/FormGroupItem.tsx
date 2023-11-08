e7d5632fe064e4db051ae09a56fdaf03534b7304
Sreenath <sreenathkumar66@gmail.com>
2023-11-08 14:19:53 +0600

    fix: remove used code from FormGroupItem

--- src/components/ui/FormGroupItem.tsx
+++ src/components/ui/FormGroupItem.tsx
@@ -1,23 +1,14 @@
-import styled from '@emotion/styled';
 import { Info } from '@mui/icons-material';
-import { Grid, TextField, Button, IconButton, } from '@mui/material'
-import Tooltip, { tooltipClasses, TooltipProps } from '@mui/material/Tooltip'
+import { Grid, TextField, IconButton, Tooltip, } from '@mui/material'
 import React, { useEffect, useState } from 'react'
 import FormInfo from './FormInfo';
 
 function FormGroupItem({ field, value, fieldInfo, disabled }: { field: formInputFieldType, value?: string, fieldInfo?: string, disabled?: boolean }) {
-   const [fieldValue, setFieldValue] = useState(value)
+   const [fieldValue, setFieldValue] = useState(value);
 
-   const HtmlTooltip = styled(({ className, ...props }: TooltipProps) => (
-      <Tooltip {...props} classes={{ popper: className }} />
-   ))(({ theme }) => ({
-      [`& .${tooltipClasses.tooltip}`]: {
-      },
-   }));
    useEffect(() => {
       setFieldValue(value)
    }, [value])
-
 
    return (
       <Grid xs item sx={{ position: 'relative', }} width={'100%'}>
