1e4f34dd13d64896eaf7d49b75cfad351d2e0947
Sreenath <sreenathkumar66@gmail.com>
2023-12-14 14:12:47 +0600

    added functions to make file into base64 and image validation on SIZE, DIMENSION and FILE EXTENSION

--- src/utils/utilitiesFn.ts
+++ src/utils/utilitiesFn.ts
@@ -18,3 +18,40 @@ export const setDynamicData = (fieldNameKey: string, fieldValue: string) => {
   })
   return val
 }
+
+export const makeBase64 = (data: string) => {
+  return `data:image/png;base64,${data}`
+}
+
+export const checkValidImage = async (file: File, maxWidth: number, maxHeight: number) => {
+  const validImageTypes = ['image/jpeg',];
+  if (!validImageTypes.includes(file.type)) {
+    return { status: 'failed', message: 'Only JPG images are allowed' };
+  }
+  if (file.size > 1 * 1024 * 1024) {
+    return { status: 'failed', message: 'Image size should be less than 1 MB' };
+  }
+  //
+  const isValidImageDimensions = await checkImageDimensions(file, maxWidth, maxHeight);
+  return isValidImageDimensions ? { status: 'success', message: 'Image is valid' } : { status: 'failed', message: `Image dimensions should be ${maxWidth}x${maxHeight}` };
+}
+
+const checkImageDimensions = (file: File, maxWidth: number, maxHeight: number) => {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    img.src = URL.createObjectURL(file);
+    try {
+      img.onload = function () {
+        event?.preventDefault();
+        // Check image dimensions
+        if (img.width == maxWidth && img.height == maxHeight) {
+          resolve(true);
+        } else {
+          resolve(false);
+        }
+      };
+    } catch (error) {
+      reject(error);
+    }
+  })
+}
