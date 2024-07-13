722a670f83d903cb938bd27d890d45caca0320f9
Sreenath Kumar <sreenathkumar66@gmail.com>
2024-07-13 15:59:53 +0600

    Feat: added sending html for analyzing, deleted previous background js and contentScript js and included gitignore file

--- webpack.config.js
+++ webpack.config.js
@@ -1,63 +1,59 @@
-const path = require('path');
-const copyWebpackPlugin = require('copy-webpack-plugin');
-const HTMLPlugin = require('html-webpack-plugin');
-const Dotenv = require('dotenv-webpack');
+const path = require("path");
+const copyWebpackPlugin = require("copy-webpack-plugin");
+const HTMLPlugin = require("html-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
 
-module.exports =
-{
-   mode: 'development',
-   devtool: 'cheap-module-source-map',
-   entry: {
-      popup: path.resolve('src/features/popup/Popup.tsx'),
-      options: path.resolve('src/features/options/Options.tsx'),
-      content: path.resolve('src/features/content/Content.ts'),
-   },
-   module: {
-      rules: [
-         { test: /\.(ts|tsx)$/, use: 'ts-loader', exclude: /node_modules/ },
-         {
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader'],
-         },
-         {
-            test: /\.(png|svg|jpg|jpeg|gif)$/i,
-            type: 'asset/resource',
-         },
-      ]
-
-   },
-   resolve: {
-      extensions: ['.tsx', '.ts', '.js']
-   },
-   plugins: [
-      new copyWebpackPlugin({
-         patterns: [
-            { from: path.resolve('src/static/'), to: path.resolve('dist') },
-         ]
-      }),
-      ...getHtmlPlugins([
-         'popup',
-         'options',
-      ]),
-      new Dotenv(),
-   ],
-   output: {
-      path: path.join(__dirname, "dist/js"),
-      filename: '[name].js'
-   },
-   // optimization: {
-   //    splitChunks: {
-   //       chunks: 'all'
-   //    }
-   // }
-}
+module.exports = {
+  mode: "development",
+  devtool: "cheap-module-source-map",
+  entry: {
+    popup: path.resolve("src/features/popup/Popup.tsx"),
+    options: path.resolve("src/features/options/Options.tsx"),
+    content: path.resolve("src/features/content/Content.ts"),
+    background: path.resolve("src/features/background/background.ts"),
+  },
+  module: {
+    rules: [
+      { test: /\.(ts|tsx)$/, use: "ts-loader", exclude: /node_modules/ },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  plugins: [
+    new copyWebpackPlugin({
+      patterns: [
+        { from: path.resolve("src/static/"), to: path.resolve("dist") },
+      ],
+    }),
+    ...getHtmlPlugins(["popup", "options"]),
+    new Dotenv(),
+  ],
+  output: {
+    path: path.join(__dirname, "dist/js"),
+    filename: "[name].js",
+  },
+  // optimization: {
+  //    splitChunks: {
+  //       chunks: 'all'
+  //    }
+  // }
+};
 function getHtmlPlugins(chunks) {
-   return chunks.map(
-      (chunk) =>
-         new HTMLPlugin({
-            title: chunk,
-            filename: `${chunk}.html`,
-            chunks: [chunk],
-         })
-   );
+  return chunks.map(
+    (chunk) =>
+      new HTMLPlugin({
+        title: chunk,
+        filename: `${chunk}.html`,
+        chunks: [chunk],
+      })
+  );
 }
